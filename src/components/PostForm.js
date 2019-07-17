865869928deda974c1cd6f666c66b8dc323d0c3a
Imita Singha <imita@psqad.com>
2019-07-17 11:49:51 +0530

    created basic application

---
+++ src/components/PostForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PostForm extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Forms</h3>
+            </div>
+        )
+    }
+}
+
+export default PostForm;
